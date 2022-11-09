83ab7ce570a754defed9117d17325cdce023f148
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-09 16:59:09 +0900

    chore : set project
    java 11
    jar

---
+++ src/test/java/com/balanceup/keum/KeumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balanceup.keum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
