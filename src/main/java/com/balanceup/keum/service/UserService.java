4d31eefd4fb0e6461e3bb43a5a03b25555ad72bb
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-14 17:39:24 +0900

    feat : add User Service

---
+++ src/main/java/com/balanceup/keum/service/UserService.java
@@ -0,0 +1,11 @@
+package com.balanceup.keum.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+}
