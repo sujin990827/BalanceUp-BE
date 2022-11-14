2360c1055718822fd0373e741cc9dd4e7040ab76
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-14 17:39:30 +0900

    feat : add User Controller

---
+++ src/main/java/com/balanceup/keum/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.balanceup.keum.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+
+}
