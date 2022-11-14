7ba47497b991f85689a0b981e16edf1239720e6b
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-14 17:38:43 +0900

    feat : add User Repository

---
+++ src/main/java/com/balanceup/keum/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.balanceup.keum.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.balanceup.keum.domain.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findByUsername(String username);
+}
