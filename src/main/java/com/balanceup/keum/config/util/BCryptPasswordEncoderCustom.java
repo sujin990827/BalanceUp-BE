79a2ce92db4f8f6cc43974370dc1be212b75ba76
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-21 17:44:55 +0900

    fix : set bean
    * 빈 순환참조 해결

---
+++ src/main/java/com/balanceup/keum/config/util/BCryptPasswordEncoderCustom.java
@@ -0,0 +1,8 @@
+package com.balanceup.keum.config.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BCryptPasswordEncoderCustom extends BCryptPasswordEncoder {
+}
