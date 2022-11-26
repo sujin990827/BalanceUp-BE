eb18e894af0a9631ae81b4fcae3a9dc03c33aa12
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-26 16:17:06 +0900

    remove : OAuth2 client dependency

--- src/main/java/com/balanceup/keum/config/SecurityConfig.java
+++ src/main/java/com/balanceup/keum/config/SecurityConfig.java
@@ -5,15 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.balanceup.keum.config.auth.PrincipalDetailService;
 import com.balanceup.keum.config.filter.CustomAccessDeniedHandler;
 import com.balanceup.keum.config.filter.CustomEntryPoint;
 import com.balanceup.keum.config.filter.JwtFilter;
-import com.balanceup.keum.config.oauth.OAuth2AuthenticationSuccessHandler;
-import com.balanceup.keum.config.oauth.PrincipalOauth2UserService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,8 +21,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	private final PrincipalOauth2UserService principalOauth2UserService;
-	private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
 	private final PrincipalDetailService principalDetailService;
 	private final CustomAccessDeniedHandler customAccessDeniedHandler;
 
@@ -46,18 +41,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			// .and()
 			.logout()
 			.logoutSuccessUrl("/")
-		// .and()
-		// .addFilterBefore(new JwtFilter(key, principalDetailService), UsernamePasswordAuthenticationFilter.class)
-		// .exceptionHandling()
-		// .authenticationEntryPoint(new CustomEntryPoint())
-		// .accessDeniedHandler(customAccessDeniedHandler)
-		// .and()
-		// .oauth2Login()
-		// .userInfoEndpoint()
-		// .userService(principalOauth2UserService)
-/*			.and()
-			.successHandler(oAuth2AuthenticationSuccessHandler)*/
-
+			.and()
+		 	.addFilterBefore(new JwtFilter(key, principalDetailService), UsernamePasswordAuthenticationFilter.class)
+		    .exceptionHandling()
+		    .authenticationEntryPoint(new CustomEntryPoint())
+			.accessDeniedHandler(customAccessDeniedHandler)
 		;
 	}
 
