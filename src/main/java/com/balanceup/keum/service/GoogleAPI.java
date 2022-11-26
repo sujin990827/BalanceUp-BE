2bd559487025aa0c57405a9814ec7ac1b9a54ee0
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-26 16:56:54 +0900

    feat : google login

--- src/main/java/com/balanceup/keum/service/GoogleAPI.java
+++ src/main/java/com/balanceup/keum/service/GoogleAPI.java
@@ -5,8 +5,6 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -24,7 +22,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
-public class KakaoAPI {
+public class GoogleAPI {
 
 	@Value("${jwt.secret-key}")
 	private String secretKey;
@@ -32,44 +30,42 @@ public class KakaoAPI {
 	@Value("${jwt.token.expired-time-ms}")
 	private Long expiredTimeMS;
 
-	private final String PROVIDER_KAKAO = "kakao";
-	private final String TOKEN_URL = "https://kauth.kakao.com/oauth/token";
-	private final String USER_INFO_URI = "https://kapi.kakao.com/v2/user/me";
+	private final String PROVIDER_GOOGLE = "google";
+	private final String TOKEN_URL = "https://oauth2.googleapis.com/token";
+	private final String USER_INFO_URI = "https://www.googleapis.com/drive/v2/files";
 	//TODO : CLIENT_ID yml 파일에 넣기
-	private final String CLIENT_ID = "66323ceffc6f43bec404a2f2addbd415";
-	private final String REDIRECT_URI = "http://localhost:8080/login/kakao";
+	private final String CLIENT_ID = "915621246164-v0qg2t6ptjk6jlj6g6hggvmnnh8ul1nd.apps.googleusercontent.com";
+	private final String CLIENT_SECRET = "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";
+	private final String REDIRECT_URI = "http://localhost:8080/login/google";
 	private final String GRANT_TYPE = "authorization_code";
 
 	private final UserRepository userRepository;
 
 	public String getAccessToken(String authorize_code) {
-		ResponseEntity<String> response = getKakaoTokenResponse(authorize_code);
+		ResponseEntity<String> response = getGoogleTokenResponse(authorize_code);
 		return getElementByResponseBody(response).getAsJsonObject().get("access_token").getAsString();
 	}
 
 	public Map<String, String> getUserInfo(String accessToken) {
-		ResponseEntity<String> response = getUserInfoResponse(setUserInfoHeaderByAccessToken(accessToken));
+		ResponseEntity<String> response = getResponseByAccessToken(accessToken);
 
 		JsonElement element = getElementByResponseBody(response);
-		JsonElement kakaoAcount = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
-
-		String password = element.getAsJsonObject().get("id").getAsString();
-		String username = kakaoAcount.getAsJsonObject().get("email").getAsString();
+		String password = element.getAsJsonObject().get("sub").getAsString();
+		String username = element.getAsJsonObject().get("email").getAsString();
 		//TODO : 임시 DB에 username + password 저장
 
 		return getHeaderUserInfo(username);
 	}
 
 	public Map<String, String> join(String username, String nickname) {
-
 		//TODO : username을 통해 임시 DB에 저장된 OAuth ID 값 찾아오기 (encoder)
-		userRepository.save(User.of(username, "Oauth ID", nickname, PROVIDER_KAKAO));
+		userRepository.save(User.of(username, "Oauth ID", nickname, PROVIDER_GOOGLE));
 		//TODO : 임시 DB 데이터 삭제
 		return makeTokens(username);
 	}
 
 	public Map<String, String> login(String username) {
-		User user = userRepository.findByUsername(username)
+		userRepository.findByUsername(username)
 			.orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 username 입니다."));
 		return makeTokens(username);
 	}
@@ -78,38 +74,37 @@ public class KakaoAPI {
 		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
 		params.add("grant_type", GRANT_TYPE);
 		params.add("client_id", CLIENT_ID);
+		params.add("client_secret", CLIENT_SECRET);
 		params.add("redirect_uri", REDIRECT_URI);
 		params.add("code", authorize_code);
 		return params;
 	}
 
-	private static HttpHeaders setHeaderContentTypeApplicationFormUrlencoded() {
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Content-type", MediaType.APPLICATION_FORM_URLENCODED_VALUE);
-		return headers;
-	}
-
-	private HttpEntity<MultiValueMap<String, String>> getKakaoTokenRequest(String authorize_code) {
+	private HttpEntity<MultiValueMap<String, String>> getOAuthTokenRequest(String authorize_code) {
 		return new HttpEntity<>(addParamByAuthorizeCode(authorize_code),
-			setHeaderContentTypeApplicationFormUrlencoded());
+			null);
 	}
 
-	private ResponseEntity<String> getKakaoTokenResponse(String authorize_code) {
-		return new RestTemplate().postForEntity(TOKEN_URL, getKakaoTokenRequest(authorize_code), String.class);
+	private ResponseEntity<String> getResponseByAccessToken(String accessToken) {
+		return new RestTemplate().postForEntity(USER_INFO_URI, getUserInfoRequest(accessToken), String.class);
+	}
+
+	private static HttpEntity<MultiValueMap<String, String>> getUserInfoRequest(String accessToken) {
+		return new HttpEntity<>(setParamByAccessToken(accessToken), null);
+	}
+
+	private static MultiValueMap<String, String> setParamByAccessToken(String accessToken) {
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("access_Token", accessToken);
+		return params;
+	}
+
+	private ResponseEntity<String> getGoogleTokenResponse(String authorize_code) {
+		return new RestTemplate().postForEntity(TOKEN_URL, getOAuthTokenRequest(authorize_code), String.class);
 	}
 
 	private JsonElement getElementByResponseBody(ResponseEntity<String> response) {
 		return new JsonParser().parse(response.getBody());
-	}
-
-	private ResponseEntity<String> getUserInfoResponse(HttpHeaders headers) {
-		return new RestTemplate().postForEntity(USER_INFO_URI, new HttpEntity<>(null, headers), String.class);
-	}
-
-	private static HttpHeaders setUserInfoHeaderByAccessToken(String accessToken) {
-		HttpHeaders headers = setHeaderContentTypeApplicationFormUrlencoded();
-		headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
-		return headers;
 	}
 
 	private Map<String, String> getHeaderUserInfo(String username) {
@@ -124,7 +119,7 @@ public class KakaoAPI {
 			return header;
 		}
 		header.put("login", "sign-up");
-		header.put("provider", PROVIDER_KAKAO);
+		header.put("provider", PROVIDER_GOOGLE);
 		return header;
 	}
 
