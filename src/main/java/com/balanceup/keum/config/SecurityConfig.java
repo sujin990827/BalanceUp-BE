2c4c348b0bea268aa2954737f4bafe08bdb673d3
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-12-06 17:58:10 +0900

    feat : add csrf filter

--- src/main/java/com/balanceup/keum/config/SecurityConfig.java
+++ src/main/java/com/balanceup/keum/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.balanceup.keum.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,13 +25,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private final PrincipalDetailService principalDetailService;
 	private final CustomAccessDeniedHandler customAccessDeniedHandler;
 	private final JwtTokenUtil jwtTokenUtil;
-
-	@Value("$jwt.secret-key")
-	private String key;
+	private final CorsConfig corsConfig;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+			.addFilter(corsConfig.corsFilter())
 			.csrf().disable()
 			.authorizeRequests()
 			.antMatchers("/admin/**").authenticated()
