b56fbe7a54e59a6c7579132c2b814488598c92d1
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-12-09 15:03:17 +0900

    test : nickname update test
    * 중복 닉네임 테스트
    1. 이미 존재하는 닉네임
    2. 정상 동작

--- src/test/java/com/balanceup/keum/service/UserServiceTest.java
+++ src/test/java/com/balanceup/keum/service/UserServiceTest.java
@@ -2,6 +2,7 @@ package com.balanceup.keum.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 import java.util.Optional;
 
@@ -14,6 +15,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.balanceup.keum.controller.dto.request.DuplicateNicknameRequest;
+import com.balanceup.keum.controller.dto.request.UpdateNicknameRequest;
 import com.balanceup.keum.domain.User;
 import com.balanceup.keum.repository.UserRepository;
 
@@ -34,10 +36,10 @@ public class UserServiceTest {
 		String nickname1 = "dog";
 		DuplicateNicknameRequest request = new DuplicateNicknameRequest(nickname1);
 
-		//when
+		//mock
 		Mockito.when(userRepository.findByNickname(any())).thenReturn(Optional.of(user));
 
-		//then
+		//when & then
 		IllegalStateException e = assertThrows(IllegalStateException.class,
 			() -> userService.duplicateNickname(request));
 		assertEquals("이미 존재하는 닉네임입니다.", e.getMessage());
@@ -47,14 +49,13 @@ public class UserServiceTest {
 	@Test
 	void given_NotDuplicateNickname_when_VerifyDuplicateNickname_then_DoesNotThrow() {
 		//given
-		User user = User.of("username", "password", "dog", "google");
 		String nickname1 = "dog";
 		DuplicateNicknameRequest request = new DuplicateNicknameRequest(nickname1);
 
-		//when
+		//mock
 		Mockito.when(userRepository.findByNickname(any())).thenReturn(Optional.empty());
 
-		//then
+		//when & then
 		assertDoesNotThrow(() -> userService.duplicateNickname(request));
 	}
 
@@ -65,8 +66,7 @@ public class UserServiceTest {
 		String nickname = null;
 		DuplicateNicknameRequest request = new DuplicateNicknameRequest(nickname);
 
-		//when
-		//then
+		//when & then
 		IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
 			() -> userService.duplicateNickname(request));
 		assertEquals("닉네임이 비어있습니다.", e.getMessage());
@@ -79,28 +79,22 @@ public class UserServiceTest {
 		String nickname = "12345678910";
 		DuplicateNicknameRequest request = new DuplicateNicknameRequest(nickname);
 
-		//when
-		//then
+		//when & then
 		IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
 			() -> userService.duplicateNickname(request));
-
-
 		assertEquals("닉네임의 길이는 11자 이내여야 합니다.", e.getMessage());
-
 	}
 
 	@DisplayName("닉네임에 숫자, 영어, 한글 외에 다른 문자가 들어가있는 경우")
 	@Test
 	void given_WrongNicknameCharacter_when_DuplicateNickname_then_ThrowException() {
 		//given
-
 		String nickname1 = "       ";
 		String nickname2 = "!@#!@$!@";
 		DuplicateNicknameRequest request1 = new DuplicateNicknameRequest(nickname1);
 		DuplicateNicknameRequest request2 = new DuplicateNicknameRequest(nickname2);
 
-		//when
-		//then
+		//when & then
 		IllegalArgumentException e1 = assertThrows(IllegalArgumentException.class,
 			() -> userService.duplicateNickname(request1));
 		IllegalArgumentException e2 = assertThrows(IllegalArgumentException.class,
@@ -108,6 +102,39 @@ public class UserServiceTest {
 
 		assertEquals("닉네임은 영어, 한글, 숫자만 가능합니다.", e1.getMessage());
 		assertEquals("닉네임은 영어, 한글, 숫자만 가능합니다.", e2.getMessage());
+	}
+
+	@DisplayName("닉네임 업데이트 테스트 - 이미 존재하는 닉네임일 경우")
+	@Test
+	void given_DuplicateNickname_when_NicknameUpdate_then_ThrowException() {
+		//given
+		String nickname = "dog";
+		UpdateNicknameRequest request = new UpdateNicknameRequest(nickname, "");
+		User user = User.of("username", "password", nickname, "google");
+
+		//mock
+		when(userRepository.findByNickname(nickname)).thenReturn(Optional.of(user));
+
+		//when & then
+		IllegalStateException e = assertThrows(IllegalStateException.class,
+			() -> userService.updateNickname(request, "username"));
+		assertEquals("이미 존재하는 닉네임입니다.", e.getMessage());
+	}
+
+	@DisplayName("닉네임 업데이트 테스트 - 존재하지 않는 닉네임일 경우")
+	@Test
+	void given_Nickname_when_NicknameUpdate_then_DoesNotThrow() {
+		//given
+		String nickname = "dog";
+		UpdateNicknameRequest request = new UpdateNicknameRequest(nickname, "");
+		User user = User.of("username", "password", nickname, "google");
+
+		//mock
+		when(userRepository.findByNickname(nickname)).thenReturn(Optional.empty());
+		when(userRepository.findByUsername(user.getUsername())).thenReturn(Optional.of(user));
+
+		//when & then
+		assertDoesNotThrow(() -> userService.updateNickname(request, "username"));
 	}
 
 }
