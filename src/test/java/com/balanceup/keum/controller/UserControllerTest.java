a3d8d9775c89533a4d9e4bdfb4b744c124315b08
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-12-12 17:36:43 +0900

    test : add token reissue controller test
    * 토큰 재발급 테스트
      * 존재하지 않는 유저(로그인이 제대로 되지 않은 유저) - IllegalStateException
      * 만료된 토큰 - IllegalStateException

--- src/test/java/com/balanceup/keum/controller/UserControllerTest.java
+++ src/test/java/com/balanceup/keum/controller/UserControllerTest.java
@@ -7,6 +7,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.util.List;
+import java.util.Map;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -15,10 +18,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.balanceup.keum.config.auth.PrincipalDetailService;
+import com.balanceup.keum.controller.dto.TokenDto;
 import com.balanceup.keum.controller.dto.request.DuplicateNicknameRequest;
 import com.balanceup.keum.controller.dto.request.UpdateNicknameRequest;
 import com.balanceup.keum.controller.dto.response.UserResponse;
@@ -37,6 +45,9 @@ public class UserControllerTest {
 
 	@MockBean
 	private UserService userService;
+
+	@MockBean
+	private PrincipalDetailService principalDetailService;
 
 	@DisplayName("[API][POST] 닉네임 중복확인 테스트 - 성공")
 	@Test
@@ -178,6 +189,77 @@ public class UserControllerTest {
 				.content(objectMapper.writeValueAsBytes(request))
 			).andDo(print())
 
+			.andExpect(status().isBadRequest())
+			.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+			.andExpect(jsonPath("$.resultCode", containsString("error")));
+	}
+
+	@DisplayName("[API][GET] 토큰 재발급 테스트 - 성공 ")
+	@Test
+	@WithMockUser
+	void given_TokenDtoAndUserDetails_when_ReIssueToken_then_ReturnCreated() throws Exception {
+		//given
+		TokenDto request = new TokenDto(Map.of("accessToken", "accessToken", "refreshToken", "refreshToken"));
+		UserDetails details =
+			new org.springframework.security.core.userdetails.User("username", "password",
+				List.of(new SimpleGrantedAuthority("ROLE_USER")));
+
+		//mock
+		when(principalDetailService.loadUserByUsername(details.getUsername())).thenReturn(mock(UserDetails.class));
+		when(userService.reIssue(request, details)).thenReturn(mock(TokenDto.class));
+
+		//when & then
+		mockMvc.perform(get("/auth/refresh")
+				.with(csrf())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsBytes(request))
+			).andDo(print())
+			.andExpect(status().isCreated())
+			.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+			.andExpect(jsonPath("$.resultCode", containsString("success")))
+			.andExpect(jsonPath("$.message", containsString("AccessToken 재발급이 완료되었습니다.")));
+	}
+
+	@DisplayName("[API][GET] 토큰 재발급 테스트(존재하지 않는 유저) - 실패 ")
+	@Test
+	@WithMockUser
+	void given_TokenDtoAndUserDetails_when_ReIssueToken_then_ReturnBadRequest() throws Exception {
+		//given
+		TokenDto request = new TokenDto(Map.of("accessToken", "accessToken", "refreshToken", "refreshToken"));
+
+		//mock
+		when(principalDetailService.loadUserByUsername(anyString()))
+			.thenThrow(UsernameNotFoundException.class);
+
+		//when & then
+		mockMvc.perform(get("/auth/refresh")
+				.with(csrf())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsBytes(request))
+			).andDo(print())
+			.andExpect(status().isBadRequest())
+			.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+			.andExpect(jsonPath("$.resultCode", containsString("error")));
+	}
+
+	@DisplayName("[API][GET] 토큰 재발급 테스트(만료토큰) - 실패 ")
+	@Test
+	@WithMockUser
+	void given_WrongToken_when_ReIssueToken_then_ReturnBadRequest() throws Exception {
+		//given
+		TokenDto request = new TokenDto(Map.of("accessToken", "accessToken", "refreshToken", "refreshToken"));
+
+		//mock
+		when(principalDetailService.loadUserByUsername(anyString())).thenReturn(mock(UserDetails.class));
+		when(userService.reIssue(Mockito.any(TokenDto.class), Mockito.any(UserDetails.class)))
+			.thenThrow(IllegalStateException.class);
+
+		//when & then
+		mockMvc.perform(get("/auth/refresh")
+				.with(csrf())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsBytes(request))
+			).andDo(print())
 			.andExpect(status().isBadRequest())
 			.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
 			.andExpect(jsonPath("$.resultCode", containsString("error")));
