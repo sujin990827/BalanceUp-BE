43e56715b9e8a8f8f97815c9f5fd507c755287f0
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-12-12 17:37:08 +0900

    feat : add constructor tokenDto

--- src/main/java/com/balanceup/keum/controller/dto/TokenDto.java
+++ src/main/java/com/balanceup/keum/controller/dto/TokenDto.java
@@ -4,8 +4,10 @@ import java.util.Map;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class TokenDto {
 
