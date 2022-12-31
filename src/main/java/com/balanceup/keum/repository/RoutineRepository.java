1dcfbe5832203935c2c7c820bfcede2810d728cb
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-12-31 14:46:11 +0900

    feat : add query
    루틴 갯수를 세주기 위한 쿼리 추가

--- src/main/java/com/balanceup/keum/repository/RoutineRepository.java
+++ src/main/java/com/balanceup/keum/repository/RoutineRepository.java
@@ -1,8 +1,12 @@
 package com.balanceup.keum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.balanceup.keum.domain.Routine;
+import com.balanceup.keum.domain.User;
 
 public interface RoutineRepository extends JpaRepository<Routine, Long> {
+	List<Routine> findAllByUser(User user);
 }
