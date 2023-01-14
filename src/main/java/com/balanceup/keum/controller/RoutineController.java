23872a5eddf973bf053dd0cfae8dc80d69caa063
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2023-01-14 15:33:55 +0900

    feat : add routine update controller
    루틴 수정 - 컨트롤러
    PUT "/routine"

--- src/main/java/com/balanceup/keum/controller/RoutineController.java
+++ src/main/java/com/balanceup/keum/controller/RoutineController.java
@@ -3,10 +3,12 @@ package com.balanceup.keum.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.balanceup.keum.controller.dto.request.routine.RoutineMakeRequest;
+import com.balanceup.keum.controller.dto.request.routine.RoutineUpdateRequest;
 import com.balanceup.keum.controller.dto.response.Response;
 import com.balanceup.keum.service.RoutineService;
 
@@ -25,6 +27,15 @@ public class RoutineController {
 				"루틴 생성이 완료되었습니다.",
 				routineService.makeRoutine(request)
 			), HttpStatus.CREATED);
+	}
+
+	@PutMapping("/routine")
+	public ResponseEntity<?> updateRoutine(@RequestBody RoutineUpdateRequest request) {
+		return new ResponseEntity<>(
+			Response.of("success",
+				"루틴 수정이 완료되었습니다.",
+				routineService.updateRoutine(request)
+			), HttpStatus.OK);
 	}
 
 }
