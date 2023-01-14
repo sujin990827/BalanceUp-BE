53be25656b2a29e0b64dffbadb893776f99d18be
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2023-01-14 15:32:44 +0900

    test : add routine update controller
    루틴 수정 테스트 - 컨트롤러

--- src/test/java/com/balanceup/keum/controller/RoutineControllerTest.java
+++ src/test/java/com/balanceup/keum/controller/RoutineControllerTest.java
@@ -19,7 +19,9 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.balanceup.keum.controller.dto.request.routine.RoutineMakeRequest;
+import com.balanceup.keum.controller.dto.request.routine.RoutineUpdateRequest;
 import com.balanceup.keum.controller.dto.response.routine.RoutineMakeResponse;
+import com.balanceup.keum.controller.dto.response.routine.RoutineResponse;
 import com.balanceup.keum.fixture.RequestFixture;
 import com.balanceup.keum.service.RoutineService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -59,13 +61,12 @@ public class RoutineControllerTest {
 			.andExpect(jsonPath("$.message", containsString("루틴 생성이 완료되었습니다.")));
 	}
 
-	@DisplayName("[API][POST] 루틴 생성 테스트 - 루틴명이 입력되지 않은 경우")
+	@DisplayName("[API][POST] 루틴 생성 테스트 - 비즈니스 로직 오류")
 	@Test
 	@WithMockUser
-	void given_NonexistentRoutineTitle_when_MakeRoutine_then_ReturnBadRequest() throws Exception {
+	void given_InvalidRequest_when_MakeRoutine_then_ReturnBadRequest() throws Exception {
 		//given
 		RoutineMakeRequest request = RequestFixture.getRoutineMakeRequestFixture();
-		request.setRoutineTitle(null);
 
 		//mock
 		when(routineService.makeRoutine(request)).thenThrow(IllegalArgumentException.class);
@@ -81,63 +82,62 @@ public class RoutineControllerTest {
 			.andExpect(jsonPath("$.resultCode", containsString("error")));
 	}
 
-	@DisplayName("[API][POST] 루틴 생성 테스트 - 루틴카테고리가 입력되지 않은 경우")
-	@Test
-	@WithMockUser
-	void given_NonexistentRoutineCategory_when_MakeRoutine_then_ReturnBadRequest() throws Exception {
-		//given
-		RoutineMakeRequest request = RequestFixture.getRoutineMakeRequestFixture();
-		request.setRoutineCategory(null);
-
-		//mock
-		when(routineService.makeRoutine(request)).thenThrow(IllegalArgumentException.class);
-
-		//when & then
-		mockMvc.perform(post("/routine")
-				.with(csrf())
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsBytes(request))
-			).andDo(print())
-			.andExpect(status().isBadRequest())
-			.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-			.andExpect(jsonPath("$.resultCode", containsString("error")));
-	}
-
-	@DisplayName("[API][POST] 루틴 생성 테스트 - 진행 요일이 입력되지 않은 경우")
-	@Test
-	@WithMockUser
-	void given_NonexistentRoutineDays_when_MakeRoutine_then_ReturnBadRequest() throws Exception {
-		//given
-		RoutineMakeRequest request = RequestFixture.getRoutineMakeRequestFixture();
-		request.setDays(null);
-
-		//mock
-		when(routineService.makeRoutine(request)).thenThrow(IllegalArgumentException.class);
-
-		//when & then
-		mockMvc.perform(post("/routine")
-				.with(csrf())
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsBytes(request))
-			).andDo(print())
-			.andExpect(status().isBadRequest())
-			.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-			.andExpect(jsonPath("$.resultCode", containsString("error")));
-	}
-
-	@DisplayName("[API][POST] 루틴 생성 테스트 - 루틴 개수가 4개를 초과한 경우")
+	@DisplayName("[API][POST] 루틴 생성 테스트 - 비즈니스 로직 오류(루틴 갯수 초과)")
 	@Test
 	@WithMockUser
 	void given_OverRoutineOfNumbers_when_MakeRoutine_then_ReturnBadRequest() throws Exception {
 		//given
 		RoutineMakeRequest request = RequestFixture.getRoutineMakeRequestFixture();
-		request.setDays(null);
+		request.setRoutineCategory(null);
 
 		//mock
 		when(routineService.makeRoutine(request)).thenThrow(IllegalStateException.class);
 
 		//when & then
 		mockMvc.perform(post("/routine")
+				.with(csrf())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsBytes(request))
+			).andDo(print())
+			.andExpect(status().isBadRequest())
+			.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+			.andExpect(jsonPath("$.resultCode", containsString("error")));
+	}
+
+	@DisplayName("[API][PUT] 루틴 수정 테스트")
+	@Test
+	@WithMockUser
+	void given_RoutineUpdateRequest_when_UpdateRoutine_then_ReturnOk() throws Exception {
+		//given
+		RoutineUpdateRequest request = RequestFixture.getRoutineUpdateRequestFixture();
+
+		//mock
+		when(routineService.updateRoutine(request)).thenReturn(Mockito.any(RoutineResponse.class));
+
+		//when & then
+		mockMvc.perform(put("/routine")
+				.with(csrf())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsBytes(request))
+			).andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+			.andExpect(jsonPath("$.resultCode", containsString("success")))
+			.andExpect(jsonPath("$.message", containsString("루틴 수정이 완료되었습니다.")));
+	}
+
+	@DisplayName("[API][PUT] 루틴 수정 테스트 - 비즈니스 로직 오류")
+	@Test
+	@WithMockUser
+	void given_InvalidRequest_when_UpdateRoutine_then_ReturnBadRequest() throws Exception {
+		//given
+		RoutineUpdateRequest request = RequestFixture.getRoutineUpdateRequestFixture();
+
+		//mock
+		when(routineService.updateRoutine(request)).thenThrow(new IllegalStateException());
+
+		//when & then
+		mockMvc.perform(put("/routine")
 				.with(csrf())
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(objectMapper.writeValueAsBytes(request))
